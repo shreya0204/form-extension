fda7a364b419401624cc27ea2ee82de2baf48927
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-11-01 14:13:01 +0530

    prompt insert in dom

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,27 @@
-(() => {
+// This function fetches and returns the content of temp.html
+function fetchTempHtml() {
+    return fetch(chrome.runtime.getURL('temp.html')).then(response => response.text());
+}
 
-    chrome.runtime.onMessage.addListener((request, sender, response) => {
+// This function injects HTML content into the page
+function injectHtml(html) {
+    const targetDivs = document.getElementsByClassName('q5O05c oydeSd');
+    if (targetDivs.length > 0) {
+        // Since getElementsByClassName returns a collection, we access the first element
+        const targetDiv = targetDivs[0];
+        const container = document.createElement('div');
+        container.innerHTML = html;
+        // Insert the container after the target div
+        targetDiv.parentNode.insertBefore(container, targetDiv.nextSibling);
+    }
+}
 
-
-
+// When the DOM is fully loaded, fetch temp.html and inject it
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+        fetchTempHtml().then(html => injectHtml(html));
     });
-
-})();
+} else {
+    // This means the DOM is already loaded
+    fetchTempHtml().then(html => injectHtml(html));
+}
